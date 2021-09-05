6cee47d2b2682a2e9e0116996d08b49180265b0f
Adel Ameri <a.ameri@live.com>
2021-09-05 14:42:43 +0430

    1400.06.14 complete employee zone employee

--- src/component/subComponent/employee_zone_employee.tsx
+++ src/component/subComponent/employee_zone_employee.tsx
@@ -6,6 +6,7 @@ import * as URL from '../../Helper/staticUrl'
 import IEmployeeZone from '../../interfaces/employee_zone'
 import IEmployee from '../../interfaces/employee'
 import IZone from '../../interfaces/zone'
+import IPost from '../../interfaces/post'
 import * as SE from '../../static/staticErrors'
 import IAlert from '../../interfaces/alert'
 
@@ -20,6 +21,42 @@ const EmployeeZoneEmployee = (props : any)=>{
         PeID : 0
     }
 
+    useEffect(()=>{
+        axios.get(URL.GetPost).then(response=>{
+            let err : string = response.data.toString()
+            if(err.includes("Kernel Error"))
+            {
+                SE.globalAlert.AlertCode = 1225
+                SE.globalAlert.Body = err
+                SE.globalAlert.Header = SE.Read
+            }else{
+                setPosts(response.data)
+            }
+        }).catch(error =>{
+            SE.globalAlert.AlertCode = 1224
+            SE.globalAlert.Body = error.toString()
+            SE.globalAlert.Header = SE.Unknown
+            onError(true, SE.globalAlert)
+        })
+
+        SE.emptyGlobalAlert()
+    },[])
+
+    const setDropDown = (zid : number) =>{
+        let opions = posts.map((value, index)=>{
+            return(
+                value.PID != 7 ?
+                <option key={index + 1} value={value.PID}>{value.PName}</option> :
+                <option key={index + 1} value={value.PID} selected>{value.PName}</option>
+            )
+        })
+        return(
+            <select id={"post_"+zid} className="form-control">
+                {opions}
+            </select>
+        )
+    }
+
     const onWaiting = (tag : boolean) =>{
         props.onWaiting(tag)
     }
@@ -30,7 +67,10 @@ const EmployeeZoneEmployee = (props : any)=>{
 
     const [emp, setEmp] = useState(employee)
     const [promptTag, setPromptTag] = useState(false)
-    const [mainTable,fillMainTable] = useState(0)
+    const [mainTable,fillMainTable] = useState(0)    
+    const [resultTable,fillResultTable] = useState(0)    
+    const [posts ,setPosts] = useState<IPost[]>([])
+    const [ezState, setEzState] = useState<IEmployeeZone[]>([])
     let tableRows : any
     let employeeZone : IEmployeeZone[]
     let zones : IZone[]
@@ -41,14 +81,66 @@ const EmployeeZoneEmployee = (props : any)=>{
     }
     
     const onSaveClick = (id : number) =>{
+        let pid  : any= $("#post_"+id+" :selected").val()
+        let employeeZone : IEmployeeZone = {
+            EFullName : "",
+            EID : emp.EID,
+            EZID : 0,
+            PID : pid,
+            PName : "",
+            ZCode : "",
+            ZID : id,
+            ZName : ""
+        }
         
+        onWaiting(true)
+        axios.post(URL.GetEmployeeZone,employeeZone).then(response =>{
+            let err : string = response.data.toString()
+            if(err.includes("Kernel Error")){
+                SE.globalAlert.AlertCode = 1211
+                SE.globalAlert.Body = err
+                SE.globalAlert.Header = SE.Create
+            }else{
+                if(response.data != "")
+                    console.log(response.data)
+            }
+        }).catch(error =>{
+            SE.globalAlert.AlertCode = 1210
+            SE.globalAlert.Body = error.toString()
+            SE.globalAlert.Header = SE.Unknown
+            onError(true, SE.globalAlert)
+        }).finally(()=>{
+            refreshTables()
+            onWaiting(false)
+        })   
+        SE.emptyGlobalAlert()
     }
 
     const onDeleteClick = (id : number) =>{
-
+        onWaiting(true)
+        axios.delete(URL.GetEmployeeZone+id).then(response => {
+            let err : string = response.data.toString()
+            if(err.includes("Kernel Error")){
+                SE.globalAlert.AlertCode = 1241
+                SE.globalAlert.Body = err
+                SE.globalAlert.Header = SE.Delete
+            }else{
+                if(response.data != "")
+                    console.log(response)
+            }
+        }).catch(error => {
+            SE.globalAlert.AlertCode = 1240
+            SE.globalAlert.Body = error.toString()
+            SE.globalAlert.Header = SE.Unknown
+            onError(true, SE.globalAlert)
+        }).finally(()=>{
+            refreshTables()
+            onWaiting(false)
+        })
+        SE.emptyGlobalAlert()
     }
 
-    const fillTableItems = () =>{
+    const fillMainTableItems = () =>{
         axios.get(URL.GetZone).then(response =>{
             let err : string = response.data.toString()
             if(err.includes("Kernel Error"))
@@ -57,13 +149,23 @@ const EmployeeZoneEmployee = (props : any)=>{
                 SE.globalAlert.Body = err
                 SE.globalAlert.Header = SE.Read
             }else{
+                let zIds : number[] = []
+                console.log("employee zone",ezState)
+                ezState.forEach((value)=>{
+                    zIds.push(value.ZID)
+                })
+                console.log("zids",zIds)
                 zones = response.data
+                zones = zones.filter((value)=>{
+                   return zIds.indexOf(value.ZID) < 0
+                })
                 tableRows = zones.map((z, index)=>
-                    <tr className="data" key={index + 1}>
+                    <tr className="data" key={z.ZID}>
                         <td>{index + 1}</td>
                         <td>{z.ZName}</td>
-                        <td>{z.ZCode}</td>                        
-                        <td><button className="btn btn-success ml-1 btn-sm">ثبت</button></td>
+                        <td>{z.ZCode}</td>
+                        <td>{setDropDown(z.ZID)}</td>                   
+                        <td><button className="btn btn-success ml-1 btn-sm" onClick={()=>{onSaveClick(z.ZID)}}>ثبت</button></td>
                     </tr>
                 )
                 fillMainTable(tableRows)
@@ -73,37 +175,62 @@ const EmployeeZoneEmployee = (props : any)=>{
             SE.globalAlert.Body = error.toString()
             SE.globalAlert.Header = SE.Unknown
             onError(true, SE.globalAlert)
+        }).finally(()=>{
+            $("#ezeSearchInput").on("keyup", function() {
+                var value = ($(this).val()!  as string).toLowerCase();
+                $("#ezeMainTable .data").filter(function() : any {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        })
+        
+        SE.emptyGlobalAlert()
+    }
+
+    const refreshTables = () =>{
+        fillResultTableItems()
+    }
+
+    const fillResultTableItems = () =>{
+        axios.get(URL.GetEmployeeZoneByEID+emp.EID).then(response =>{
+            let err : string = response.data.toString()
+            if(err.includes("Kernel Error"))
+            {
+                SE.globalAlert.AlertCode = 1223
+                SE.globalAlert.Body = err
+                SE.globalAlert.Header = SE.Read
+            }else{
+                employeeZone = response.data
+                setEzState(employeeZone)
+                tableRows = employeeZone.map((z, index)=>
+                    <tr className="data" key={index + 1}>
+                        <td>{index + 1}</td>
+                        <td>{z.ZName}</td>
+                        <td>{z.ZCode}</td>
+                        <td>{z.PName}</td>                   
+                        <td><button className="btn btn-danger btn-sm" onClick={()=>{onDeleteClick(z.EZID)}}>حذف</button></td>
+                    </tr>
+                )
+                fillResultTable(tableRows)
+            }          
+        }).catch(error =>{
+            SE.globalAlert.AlertCode = 1222
+            SE.globalAlert.Body = error.toString()
+            SE.globalAlert.Header = SE.Unknown
+            onError(true, SE.globalAlert)
         })
         
         SE.emptyGlobalAlert()
     }
 
     useEffect(()=>{
-        //#region search table function 
-        $("#ezeSearchInput").on("keyup", function() {
-            var value = ($(this).val()!  as string).toLowerCase();
-            $("#ezeMainTable .data").filter(function() : any {
-                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
-        });
-        //#endregion
-
-    },[])
-
-    useEffect(()=>{
         setPromptTag(props.ezTag)
-        if(props.ezTag){
-            //#region add all workgroup items
-            
-            //#endregion
-
-            
-            //#region fill employee table        
-            fillTableItems()
-            //#endregion
-        }
         
     },[props.ezTag])
+
+    useEffect(()=>{        
+        fillMainTableItems()
+    },[ezState])
 
     //#region get employee information from server
     useEffect(()=>{
@@ -125,6 +252,15 @@ const EmployeeZoneEmployee = (props : any)=>{
         )
         
     },[promptTag])
+    //#endregion
+
+    //#region refresh tables
+    useEffect(()=>{
+        if(emp.EID >0){
+            refreshTables()
+        }
+
+    },[emp])
     //#endregion
 
     //#region set body html
@@ -167,22 +303,12 @@ const EmployeeZoneEmployee = (props : any)=>{
                                         <th>ردیف</th>
                                         <th>نام حوزه</th>
                                         <th>کد حوزه</th>
+                                        <th>پست سازمانی</th>
                                         <th>عملیات</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr className="data">
-                                        <td>1</td>
-                                        <td>مشاغل 03</td>
-                                        <td>2303</td>
-                                        <td><button className="btn btn-danger btn-sm">حذف</button></td>
-                                    </tr>
-                                    <tr className="data">
-                                        <td>2</td>
-                                        <td>شرکت ها 17</td>
-                                        <td>2317</td>
-                                        <td><button className="btn btn-danger btn-sm">حذف</button></td>
-                                    </tr>
+                                   {resultTable}
                                 </tbody>
                             </table>
                         </div>
@@ -193,6 +319,7 @@ const EmployeeZoneEmployee = (props : any)=>{
                                         <th>ردیف</th>
                                         <th>نام حوزه</th>
                                         <th>کد حوزه</th>
+                                        <th>پست سازمانی</th>
                                         <th>عملیات</th>
                                     </tr>
                                 </thead>
